0d0700ad2383a62a55bb4cbd462cbb1aff6ecc59
SrWillxD <willgra09@hotmail.com>
2023-06-04 17:18:41 -0300

    adicionando testes de abertura de campo

--- src/test/java/br/com/cod3r/campoMinado/modelo/CampoTest.java
+++ src/test/java/br/com/cod3r/campoMinado/modelo/CampoTest.java
@@ -89,6 +89,35 @@ public class CampoTest {
         });
     }
 
+    @Test
+    void testeAbrirComVizinhos1(){
+        Campo campo11 = new Campo(1,1);
+        Campo campo22 = new Campo(2, 2);
+
+        campo22.adicionarVizinho(campo11);
+        campo.adicionarVizinho(campo22);
+
+        campo.abrir();
+
+        assertTrue(campo22.isAberto() && campo11.isAberto());
+    }
+    @Test
+    void testeAbrirComVizinhos2(){
+        Campo campo11 = new Campo(1,1);
+        Campo campo12 = new Campo(1,1);
+        Campo campo22 = new Campo(2, 2);
+
+        campo12.minar();
+
+        campo22.adicionarVizinho(campo11);
+        campo22.adicionarVizinho(campo12);
+
+        campo.adicionarVizinho(campo22);
+
+        campo.abrir();
+
+        assertTrue(campo22.isAberto() && campo11.isFechado());
+    }
 
 
 }
