8172eca107ad0e49e6ba609ec6df0225589aed8d
SrWillxD <willgra09@hotmail.com>
2023-06-05 10:50:52 -0300

    adicionando um construtor e os metodos gerarCampos, associarVizinhos, sortearMinas

--- src/main/java/br/com/cod3r/campoMinado/modelo/Tabuleiro.java
+++ src/main/java/br/com/cod3r/campoMinado/modelo/Tabuleiro.java
@@ -9,5 +9,33 @@ public class Tabuleiro {
     private int minas;
     private final List<Campo> campos = new ArrayList();
 
+    public Tabuleiro(int linhas, int colunas, int minas){
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+
+    }
+
+    private void gerarCampos(){
+        for(int linha = 0; linha < linhas; linha++){
+            for(int colunas = 0; colunas < this.colunas; colunas++){
+                campos.add(new Campo(linha, colunas));
+            }
+        }
+    }
+
+    private void associarVizinhos(){
+
+    }
+
+    private void sortearMinas(){
+        
+    }
+
+
 
 }
