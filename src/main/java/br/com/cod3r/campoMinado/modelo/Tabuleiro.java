8cc4bb3c6cffe9f5da2626634737c3a223f39b24
SrWillxD <willgra09@hotmail.com>
2023-06-05 10:28:19 -0300

    atributos iniciais do tabuleiro

--- src/main/java/br/com/cod3r/campoMinado/modelo/Tabuleiro.java
+++ src/main/java/br/com/cod3r/campoMinado/modelo/Tabuleiro.java
@@ -1,4 +1,13 @@
 package br.com.cod3r.campoMinado.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int minas;
+    private final List<Campo> campos = new ArrayList();
+
+
 }
